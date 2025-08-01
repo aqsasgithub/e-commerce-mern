2f5d7cdbc1f7de4941d90df49a7a5d2535520b9e
Aqsa Syed <aqsas2774@gmail.com>
2025-08-01 22:34:30 +0500

    Backend URL added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/" : "http://localhost:3000",
+      "/api/": "https://e-commerce-mern-b4ef.onrender.com",
+      "/uploads/" : "https://e-commerce-mern-b4ef.onrender.com",
     }
   }
 })
